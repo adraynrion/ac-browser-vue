062b03c03bbcfbd94ef47981942887b240266335
Florian GRONDIN <grondin.florian@protonmail.com>
2022-09-16 21:42:12 +0200

    Init commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.scss";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
