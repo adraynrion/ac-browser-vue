f5125e0184cddac806fd43ae62d85790a48e90d6
Florian GRONDIN <grondin.florian@protonmail.com>
2022-12-25 08:05:52 +0100

    Simple MainTopbar implemented

---
+++ src/components/MainTopbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="d-flex justify-content-between">
+    <div>
+      <img src="@/assets/ac-title.png" alt="Assassin's Creed title" />
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  img {
+    height: 5rem;
+  }
+}
+</style>
